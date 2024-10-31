ddc894b3b8e630248a680b1f6d756f228cc9982e
Warrick Tsui <warricktsui1226@gmail.com>
2024-10-30 21:57:20 -0400

    More icons added to the website

--- warrick-portfolio/src/components/Sidebar/index.js
+++ warrick-portfolio/src/components/Sidebar/index.js
@@ -1,8 +1,8 @@
-import './index.scss'
-import {Link, NavLink} from 'react-router-dom'
+import { faEnvelope, faGithub, faHome, faLinkedin, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link, NavLink } from 'react-router-dom'
 import WarrickGraduation from '../../assets/images/Graduation.png'
-import {faHome} from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import './index.scss'
 
 const Sidebar =() =>(
     <div className = 'nav-bar'>
@@ -13,8 +13,33 @@ const Sidebar =() =>(
             <NavLink exact = "true" activeclassname = "active" to = "/">
                 <FontAwesomeIcon icon={faHome} color = "#59218a"/>
             </NavLink>
+            <NavLink exact = "true" activeclassname = "active" className = 'education-link' to = "/education">
+                <FontAwesomeIcon icon={faUser} color = "#59218a"/>
+            </NavLink>
+            <NavLink exact = "true" activeclassname = "active" className = 'projects-link' to = "/projects">
+                <FontAwesomeIcon icon={faEnvelope} color = "#59218a"/>
+            </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href = 'https://www.linkedin.com/in/warrick-tsui-1a83772a3/'>
+                    <FontAwesomeIcon icon={faLinkedin} color = "#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href = "https://www.linkedin.com/in/warrick-tsui-1a83772a3/">
+                    <FontAwesomeIcon icon={faGithub} color = "#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href = "https://www.linkedin.com/in/warrick-tsui-1a83772a3/">
+                    <FontAwesomeIcon icon={faLinkedin} color = "#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
     </div>
 )
+
+/*Change these icons into a long sidebar */
 
 export default Sidebar
